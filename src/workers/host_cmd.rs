00f4a903917ab83ad7a48830b8c9abc1b07bd539
Itta Funahashi <itta@itta.dev>
2023-10-08 20:47:24 +0900

    feat: show chars in lcd

--- src/workers/host_cmd.rs
+++ src/workers/host_cmd.rs
@@ -2,6 +2,7 @@ use crate::types::IndexResponse;
 use crate::utils::get_ip_address;
 use actix_web::*;
 use clap::Parser;
+use rppal;
 
 #[derive(Parser)]
 pub struct HostArg {
@@ -22,6 +23,13 @@ async fn index() -> impl Responder {
 }
 
 pub async fn run(arg: &HostArg) -> std::io::Result<()> {
+    const I2C_ADDRESS: u16 = 0x27;
+    let mut i2c = I2c::with_bus(1)?;
+    let buffer: &[u8] = b"Hello, World!";
+    let bytes_written = i2c.write(buffer)?;
+
+    println!("{} bytes written to the LCD", bytes_written);
+
     if let Ok(ip_address) = get_ip_address::get() {
         println!("Started host server at http://{}:{}", ip_address, arg.port);
     }
